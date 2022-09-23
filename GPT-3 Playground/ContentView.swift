11120d7f1c9861515434a556867cfed71cf36124
Jed Fox <git@jedfox.com>
2022-09-22 20:17:30 -0400

    Initial Commit

---
+++ GPT-3 Playground/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GPT-3 Playground
+//
+//  Created by Jed Fox on 2022-09-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GPT-3 Playground/GPT_3_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPT_3_PlaygroundApp.swift
+//  GPT-3 Playground
+//
+//  Created by Jed Fox on 2022-09-22.
+//
+
+import SwiftUI
+
+@main
+struct GPT_3_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
