c312864ef82758e59e7a8454534693efc03a983f
Jed Fox <git@jedfox.com>
2023-04-18 23:57:03 -0400

    Update default prompt & settings, cleanup unused model setting

--- LLaMA Playground/LLaMAConfig.swift
+++ LLaMA Playground/LLaMAConfig.swift
@@ -10,21 +10,13 @@ import Defaults
 
 struct Configuration: Codable, Defaults.Serializable {
     var seed: Int32 = -1
-    var prompt = "Write a tagline for an ice cream shop."
+    var prompt = "### Human: What is the meaning of life?\n### Assistant:"
     var threads: Int32 = 4
-    var tokens = 128
+    var tokens = 2048
     var topK = 40
-    var topP = 0.9
-    var temperature = 0.8
-    var batchSize = 8
-    var model = Model.seven
+    var topP = 1.0
+    var temperature = 0.19
+    var batchSize = 32
     var repeatWindow = 64
     var repeatPenalty = 1.3
-
-    enum Model: String, Codable, CaseIterable {
-        case seven
-        case thirteen
-        case thirty
-        case sixtyFive
-    }
 }
